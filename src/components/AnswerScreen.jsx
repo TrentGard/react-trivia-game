b78f5134e5719267d3e4256d5612d5fbeaf68aa9
Trent Gard <trentgard@gmail.com>
2019-07-10 19:23:43 -0500

    mobile css work

--- src/components/AnswerScreen.jsx
+++ src/components/AnswerScreen.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../App.css";
 import gifArray from "../gifArray.json";
 import questions from "../questionsArray.json";
 
@@ -6,7 +7,11 @@ function AnswerScreen(props) {
   return (
     <div>
       <h3>Your Answer: {props.userAnswer}</h3>
-      <img src={gifArray[props.currentQuestion]} alt="Colts" />
+      <img
+        src={gifArray[props.currentQuestion]}
+        alt="Colts"
+        className="image"
+      />
       <h3>Correct Answer: {questions[props.currentQuestion].correctAnswer}</h3>
     </div>
   );
